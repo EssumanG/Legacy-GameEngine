4e5f394a9fd204ebab4e980cca76d963e0c1591e
Essuman Godsaves <kojosayves7@gmail.com>
2023-05-03 06:05:43 +0000

    included logging system

--- include/Hazel/hazel.h
+++ include/Hazel/hazel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../src/Application.h"
+#include "../../src/log.h"
 
 /*-----------Entry Point---------*/
 #include "../../src/EntryPoint.h"
--- src/EntryPoint.h
+++ src/EntryPoint.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Application.h"
+#include "log.h"
 
 namespace Hazel{
 
@@ -10,6 +11,10 @@ extern Application* CreateApplication();
 
 int main(int argc, char** argv)
 {
+    Hazel::log::Init();
+    HZ_CORE_WARN("INITIALIZED LOG!");
+    int a = 5;
+    HZ_INFO("HELLO! Var ={0}", a);
     auto app = Hazel::CreateApplication();
     app->Run();
     delete app;
